34b08951f7387322ab959594dced76596684341b
LidiiaChernysh <73900043+LidiiaChernysh@users.noreply.github.com>
2020-11-12 16:42:22 +0200

    Add files via upload

---
+++ create_fact_tripdata.sql
@@ -0,0 +1,5 @@
+CREATE TABLE chernysh_schema.fact_tripdata
+WITH (  CLUSTERED COLUMNSTORE INDEX,
+        DISTRIBUTION =  HASH([DOLocationID])
+    ) AS
+SELECT  * FROM    chernysh_schema.external_table_chernysh
