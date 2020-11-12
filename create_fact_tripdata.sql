e037ab8fa6e414d20163081222c5862e7c7df24a
LidiiaChernysh <73900043+LidiiaChernysh@users.noreply.github.com>
2020-11-12 17:32:47 +0200

    Delete create_fact_tripdata.sql

--- create_fact_tripdata.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE chernysh_schema.fact_tripdata
-WITH (  CLUSTERED COLUMNSTORE INDEX,
-        DISTRIBUTION =  HASH([DOLocationID])
-    ) AS
-SELECT  * FROM    chernysh_schema.external_table_chernysh
